7fff1f59f53d31989f56bb61b47fccb004392bbc
Greg Lind <greglind@gregs-air.lan>
2022-01-26 15:25:48 -0800

    env

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -105,7 +105,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'cms',
-        'PASSWORD': '9IIaOVhIoBgQNyNG',
+        'PASSWORD': os.environ["PASSWORD"],
         'USER': 'cms_user',
         'HOST': 'db-mysql-nyc3-97229-do-user-2508039-0.b.db.ondigitalocean.com',
         'PORT': '25060',
