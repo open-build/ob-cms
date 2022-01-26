cb1782caac269876b1f99872db85825359898035
Greg Lind <greglind@gregs-air.lan>
2022-01-26 13:04:51 -0800

    connect to prod

--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -1,5 +1,16 @@
 from .base import *
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'cms',
+        'PASSWORD': '9IIaOVhIoBgQNyNG',
+        'USER': 'cms_user',
+        'HOST': 'db-mysql-nyc3-97229-do-user-2508039-0.b.db.ondigitalocean.com',
+        'PORT': '25060',
+    }
+}
+
 DEBUG = False
 
 try:
