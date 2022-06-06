4896444cf9792a8d18bd52d1b2dcb41cdec463cd
Greg Lind <greglind@gregs-air.lan>
2022-06-06 15:39:36 -0700

    fix failed migrations

--- home/migrations/0008_fundidora_nosotros_novedades_portafolio.py
+++ home/migrations/0008_fundidora_nosotros_novedades_portafolio.py
@@ -10,3 +10,36 @@ class Migration(migrations.Migration):
         ('wagtailcore', '0066_collection_management_permissions'),
         ('home', '0007_inicio'),
     ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Nosotros',
+            fields=[
+                ('page_ptr', models.OneToOneField(auto_created=True, on_delete=django.db.models.deletion.CASCADE, parent_link=True, primary_key=True, serialize=False, to='wagtailcore.page')),
+            ],
+            options={
+                'abstract': False,
+            },
+            bases=('wagtailcore.page',),
+        ),
+        migrations.CreateModel(
+            name='Novedades',
+            fields=[
+                ('page_ptr', models.OneToOneField(auto_created=True, on_delete=django.db.models.deletion.CASCADE, parent_link=True, primary_key=True, serialize=False, to='wagtailcore.page')),
+            ],
+            options={
+                'abstract': False,
+            },
+            bases=('wagtailcore.page',),
+        ),
+        migrations.CreateModel(
+            name='Portafolio',
+            fields=[
+                ('page_ptr', models.OneToOneField(auto_created=True, on_delete=django.db.models.deletion.CASCADE, parent_link=True, primary_key=True, serialize=False, to='wagtailcore.page')),
+            ],
+            options={
+                'abstract': False,
+            },
+            bases=('wagtailcore.page',),
+        ),
+    ]
