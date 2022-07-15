5c86e320eef61e86a59975ce2ef38740ebe2f2e6
Greg Lind <greglind@gregs-air.lan>
2022-07-15 10:10:33 -0700

    added content types for buildly

--- home/models.py
+++ home/models.py
@@ -6,29 +6,14 @@ from wagtail.core.models import Page
 class HomePage(Page):
     pass
 
-class AboutPage(Page):
+class DevelopersPage(Page):
     pass
 
-class Portfolio(Page):
+class AboutPage(Page):
     pass
 
 class Foundry(Page):
     pass
 
 class Contact(Page):
-    pass
-
-class Inicio(Page):
-    pass
-
-class Nosotros(Page):
-    pass
-
-class Portafolio(Page):
-    pass
-
-class Novedades(Page):
-    pass
-
-class Fundidora(Page):
     pass
