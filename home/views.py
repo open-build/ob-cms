7b70b31f6322ec164b350abb328dbf4cdebb44f5
Greg Lind <greglind@gregs-air.lan>
2022-10-12 15:01:20 -0700

    community

--- home/views.py
+++ home/views.py
@@ -23,11 +23,3 @@ def contactView(request):
 
 def successView(request):
     return HttpResponse("Success! Thank you for your message.")
-
-#redirect to spanish if the url contains colombia.open.build
-def index(req):
-    sub, _ = req.META['HTTP_HOST'].split('.', 1)
-    if sub == 'colombia':
-        return HttpResponseRedirect("/inicio")
-    else:
-        return HttpResponseRedirect("/")
