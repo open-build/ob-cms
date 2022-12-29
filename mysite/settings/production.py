8b0a134cb33c5022d3c16e52089b1cb618ad4a50
glind <greg@buildly.io>
2022-12-29 14:00:16 -0800

    update production mode and images in news

--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -21,3 +21,30 @@ try:
     from .local import *
 except ImportError:
     pass
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"  # new
+DEFAULT_FROM_EMAIL = "help@open.build"
+EMAIL_HOST = "smtp.sendgrid.net"  # new
+EMAIL_HOST_USER = "apikey"  # new
+EMAIL_HOST_PASSWORD = os.environ.get("SENDGRID_PASSWORD")  # new
+EMAIL_PORT = 587  # new
+EMAIL_USE_TLS = True  # new
+
+AWS_STORAGE_BUCKET_NAME = 'open-build'
+AWS_ACCESS_KEY_ID = 'DO00MW9V6QPPJKVCGHYA'
+AWS_SECRET_ACCESS_KEY = os.environ.get("SPACES_SECRET")
+AWS_S3_CUSTOM_DOMAIN = 'cms-static.nyc3.digitaloceanspaces.com' + "/" + AWS_STORAGE_BUCKET_NAME
+AWS_S3_ENDPOINT_URL  = 'https://cms-static.nyc3.digitaloceanspaces.com'
+
+
+MEDIA_URL = AWS_S3_CUSTOM_DOMAIN + "/" + AWS_STORAGE_BUCKET_NAME + "/"
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+AWS_LOCATION = 'static'
+STATIC_URL = f'https://{AWS_S3_ENDPOINT_URL}/{AWS_STORAGE_BUCKET_NAME}/{AWS_LOCATION}/'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
+
+AWS_DEFAULT_ACL = 'public-read'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
